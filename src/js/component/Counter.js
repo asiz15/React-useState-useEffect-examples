1f54c089c059c1a8c5cac50663f17ef9d0436aac
Alex Silva Z <a.silvazapata@gmail.com>
2021-08-30 19:30:54 +0000

    set component for explanations

---
+++ src/js/component/Counter.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+
+export const Counter = ({ from }) => {
+	const [counter, setCounter] = useState(from);
+	const [text, setText] = useState("Some text");
+
+	const decrease = () => {
+		setCounter(counter - 1);
+	};
+
+	const randomText = () => {
+		const arr = [
+			"Hi world",
+			"Hello world",
+			"4Geeks",
+			"Javascript",
+			"React"
+		];
+		const item = arr[Math.floor(Math.random() * arr.length)];
+
+		setText(item);
+	};
+
+	useEffect(() => {
+		console.log("This will be printed only if 'counter' changes");
+		const timer = counter > 0 && setInterval(() => decrease(), 1000);
+		return () => clearInterval(timer);
+	}, [counter]);
+
+	useEffect(() => {
+		console.log("This will be printed only if 'text' has changes");
+		console.log(`'text' is now ${text}`);
+	}, [text]);
+
+	return (
+		<div>
+			<h1>{counter}</h1>
+			<p>{text}</p>
+			<button onClick={() => setCounter(counter + 1)}>+</button>
+			<button onClick={decrease}>-</button>
+			<br></br>
+			<button onClick={randomText}>Change text</button>
+		</div>
+	);
+};
+
+Counter.propTypes = {
+	from: PropTypes.number
+};
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import { Counter } from "./Counter";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [counter, setCounter] = useState(33);
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<Counter from={counter}></Counter>
 		</div>
 	);
 };
